4cc794ebbb895d47a22b57cf5a81a1b77fa0e625
jastidinesh <jasti_dinesh@yahoo.com>
2015-05-27 17:04:25 -0700

    Made Changes To Avg-DBChanges

--- src/main/java/com/DBConnect.java
+++ src/main/java/com/DBConnect.java
@@ -1,14 +1,15 @@
 package com;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBConnect {
  
 	public   Connection get_connection() {
 
-	 /*Connection connection = null;
+	 Connection connection = null;
 	 URI dbUri;
 	try {
 		dbUri = new URI(System.getenv("DATABASE_URL"));
@@ -30,7 +31,7 @@ public class DBConnect {
 	}
 
 
-	    return connection;*/
+	    return connection;/*
 		try {
 		    Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
@@ -48,6 +49,6 @@ public class DBConnect {
 		} catch (SQLException e) {
 		    e.printStackTrace();
 		}
-		return connection;
+		return connection;*/
 	}
 }
